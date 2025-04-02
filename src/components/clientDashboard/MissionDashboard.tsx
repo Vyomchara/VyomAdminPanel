e71f1eeabbc5f0cb340507843cd3a2bddec70977
me-vaibhav <164721792+me-vaibhav@users.noreply.github.com>
2025-04-02 20:13:45 +0530

    no changes

--- src/components/clientDashboard/MissionDashboard.tsx
+++ src/components/clientDashboard/MissionDashboard.tsx
@@ -4,7 +4,7 @@ import { FileGallery } from "@/components/clientDashboard/FileGallery"
 
 export function MissionDashboard({ client, clientId }: { client?: any, clientId: string }) {
   return (
-    <div className="space-y-8">
+    <div className="space-y-8 mt-8">
       <FileGallery 
         clientId={clientId}
       />
